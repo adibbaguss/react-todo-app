ba6ecf6c8c195a5c11fb9c8fb5fe4451588797cf
AdibBaguss <adibbagus42@gmail.com>
2024-06-05 14:46:41 +0700

    Develop add todo feature

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import reactLogo from './assets/react.svg';
 import viteLogo from '/vite.svg';
 import './App.css';
 import Todos from './components/Todos'; // Lalukan Import
-
+import TodoForm from './components/TodoForm'; // import TodoForm
 function App() {
   const [todos, setTodos] = useState([
     {
@@ -36,9 +36,28 @@ function App() {
     setTodos(updatedTodos);
   };
 
+  // mendefinisikan function addTodo
+  const addTodo = (todoTitle) => {
+    if (todoTitle === '') {
+      return;
+    }
+
+    const newTodo = {
+      id: todos.length + 1,
+      title: todoTitle,
+      completed: false,
+    };
+
+    const updatedTodos = todos.concat(newTodo);
+    setTodos(updatedTodos);
+  };
+
   return (
     <div style={styles.container}>
       <h1 style={styles.title}>My Todo List</h1>
+      {/* menampilkan component Todo */}
+      {/* Teruskan function addTodo sebagai props */}
+      <TodoForm addTodo={addTodo} />
       {/* teruskan function toggleCompleted ke component Todos */}
       <Todos todos={todos} toggleCompleted={toggleCompleted} />
     </div>
